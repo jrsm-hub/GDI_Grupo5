4801ad5740d7ef18d1c2925d4a983e18cccda22f
jrsm-hub <jrsm@cin.ufpe.br>
2022-08-29 20:31:25 -0300

    primeiras tabelas

--- Entrega 1/tabelas.sql
+++ Entrega 1/tabelas.sql
@@ -1,1 +1,132 @@
 --Criação das tabelas
+
+--Pessoa 
+CREATE TABLE pessoa (
+    cpf VARCHAR2 (5),
+    nome VARCHAR2 (255) NOT NULL,
+    sexo CHAR,
+    idade NUMBER NOT NULL,
+    cep VARCHAR2 (5) NOT NULL,
+    numero NUMBER NOT NULL,
+    complemento VARCHAR2 (255) NOT NULL,
+
+    CONSTRAINT pessoa_pkey PRIMARY KEY (cpf),
+    CONSTRAINT pessoa_checkGenero CHECK (sexo = 'M' OR sexo = 'F')
+    
+);
+
+
+--Cliente
+CREATE TABLE cliente (
+    cpf_c VARCHAR2 (5),
+    peso NUMBER NOT NULL,
+    altura TINYINT (255),
+    percentual_gordura NUMBER,
+    imc NUMBER NOT NULL,
+    biotipo VARCHAR2(4),
+    plano_saude VARCHAR2(255) NOT NULL,
+
+    CONSTRAINT cliente_pkey PRIMARY KEY (cpf_c),
+    CONSTRAINT cliente_fkey FOREIGN KEY (cpf_c) REFERENCES pessoa (cpf),
+    CONSTRAINT cliente_checkPercent CHECK (percentual_gordura >= 0 AND percentual_gordura < 100),
+    CONSTRAINT cliente_checkBiotipo CHECK (biotipo in ('ECTO', 'ENDO', 'MESO'))
+    
+);
+
+--Funcionario 
+CREATE TABLE funcionario (
+    cpf_fssss,
+    data_admissao DATE NOT NULL,
+    cargo VARCHAR2 (255) NOT NULL,
+    salario NUMBER (*,2),
+    cpf_ger VARCHAR2 (5),
+    
+    CONSTRAINT funcionario_check (salario >= 1212.00),
+    CONSTRAINT funcionario_pkey PRIMARY KEY (cpf_f),
+    CONSTRAINT funcionario_fkey FOREIGN KEY (cpf_ger) REFERENCES funcionario (cpf_f)
+);
+--Nutricionista
+CREATE TABLE nutricionista (
+    crn VARCHAR2 (5) NOT NULL,
+    cpf_n VARCHAR2 (5) NOT NULL,
+    
+    CONSTRAINT nutricionista_pkey PRIMARY KEY (crn)
+    CONSTRAINT nutricionista_fkey FOREIGN KEY (cpf_n) REFERENCES funcionario (cpf_f)
+
+)
+--Vendedor
+CREATE TABLE vendedor (
+    cpf_v VARCHAR2 (5) NOT NULL,
+    cod_vendedor VARCHAR2 (3) NOT NULL,
+    
+    CONSTRAINT vendedor_pkey PRIMARY KEY (cpf_v),
+    CONSTRAINT vendedor_fkey FOREIGN KEY (cpf_v) REFERENCES funcionario(cpf_f)
+    
+);
+--Atendente
+CREATE TABLE atendente (
+    Cod_Atendente VARCHAR2 (3) NOT NULL,
+    cpf_a CHAR CHAR(5) NOT NULL,
+    
+    CONSTRAINT atendente_pkey PRIMARY KEY (cpf_a)
+    CONSTRAINT atendente_fkey FOREIGN KEY (cpf
+a) REFERENCES funcionario(cpf_f)
+
+);
+--Fabricante
+CREATE TABLE fabricante (
+  cnpj VARCHAR2(5) NOT NULL,
+  nome_fabri VARCHAR2 (255) NOT NULL,
+
+  CONSTRAINT fabricante_pkey PRIMARY KEY (cnpj)
+  
+
+);
+--Produto
+CREATE TABLE produto(
+    cnpj_f VARCHAR2(5),
+    nome_p VARCHAR2 (255),
+    valor_compra NUMBER NOT NULL,
+    valor_revenda NUMBER NOT NULL,
+    estoque NUMBER NOT NULL,
+    data_fabricacao DATE NOT NULL,
+    data_vencimento DATE NOT NULL,
+
+    CONSTRAINT produto_fkey FOREIGN KEY (cnpj_f) REFERENCES fabricante(cnpj)
+    CONSTRAINT produto_pkey PRIMARY KEY (cnpj_f, nome_p)
+);
+
+--Consulta
+CREATE TABLE consulta (
+    cpf_c VARCHAR2 (5) NOT NULL,
+    crn VARCHAR2 (4) NOT NULL,
+    data_hora_consulta TIMESTAMP NOT NULL,
+
+    CONSTRAINT consulta_pkey PRIMARY KEY (cpf_c, crn),
+    CONSTRAINT consulta_fkey FOREIGN KEY (cpf_c) REFERENCES cliente (cpf_c)
+
+);
+--Marcar_Consulta
+CREATE TABLE (
+    cpf_c VARCHAR2 (5) NOT NULL,
+    crn VARCHAR2 (4) NOT NULL,
+
+);
+
+CREATE TABLE marcar_consulta (
+    cpf_c VARCHAR2(5) NOT NULL,
+    crn VARCHAR2(5) NOT NULL,
+    Cod_Atendente VARCHAR2(5) NOT NULL,
+    data_hora_marcada TIMESTAMP NOT NULL,
+
+    CONSTRAINT marcar_consulta_cpf_fkey FOREIGN KEY (cpf_c) REFERENCES cliente (cpf_c),
+    CONSTRAINT marcar_consulta_crn_fkey FOREIGN KEY (crn) REFERENCES nutricionista (crn),
+    CONSTRAINT marcar_consulta_cod_fkey FOREIGN KEY (Cod_Atendente) REFERENCES atendente (Cod_Atendente)
+    
+    
+);
+--Prescreve
+
+--Compra
+
+--Telefone
